cc32dc0ec7c732812c091008bdeb8a815b47c745
clairecasalnova <ccc2876@rit.edu>
2019-11-05 09:54:48 -0500

    updated code, math should be correct now

--- secretsharing.py
+++ secretsharing.py
@@ -4,52 +4,50 @@ from sys import maxsize
 
 class Aggregator:
     def __init__(self, ID):
-        self.ID=ID
-        self.shares_list=[]
-        self.running_total=0
-        self.current_total=0
+        self.ID = ID
+        self.shares_list = []
+        self.running_total = 0
+        self.current_total = 0
 
     def get_ID(self):
         return self.ID
 
     def print_shares_list(self):
-        shares=""
+        shares = ""
         for s in self.shares_list:
-            shares+=str(s)
-            shares+=", "
+            shares += str(s)
+            shares += ", "
         return shares
-
 
 
 class SmartMeter:
 
-    def __init__(self,degree, secret, ID):
-        self.ID=ID
-        self.degree= degree
-        self.secret=secret
-        self.polynomial=""
-        self.coeff_list=[]
+    def __init__(self, degree, secret, ID):
+        self.ID = ID
+        self.degree = degree
+        self.secret = secret
+        self.polynomial = ""
+        self.coeff_list = []
+        self.bin_string =[]
 
     def get_ID(self):
         return self.ID
 
-
     def create_poly(self):
         # create a string of binary numbers for the x values
-        bin_string = [1]
-
+        self.bin_string.append(1)
 
         # loop over the range of the degree to generate whether the power of x will be present
         for i in range(1, self.degree):
             bit = random.randint(0, 1)
-            bin_string.append(bit)
+            self.bin_string.append(bit)
         power = self.degree
         polystring = ""
 
         # loop over the binary string and generate the coefficients
-        for i in range(0, len(bin_string)):
-            if bin_string[i] == 1:
-                coeff = random.randint(0, maxsize)
+        for i in range(0, len(self.bin_string)):
+            if self.bin_string[i] == 1:
+                coeff = random.randint(1, maxsize)
                 polystring += str(coeff) + "x^" + str(power) + "+"
                 self.coeff_list.append(coeff)
             else:
@@ -58,28 +56,24 @@ class SmartMeter:
 
         # add the secret to the end of the polynomial as the constant
         polystring += str(self.secret)
-        self.polynomial=polystring
+        self.polynomial = polystring
 
+    def create_shares(self, agg):
+        length = len(self.coeff_list)
+        power = self.degree
 
-    def create_shares(self,agg):
-        length=len(self.coeff_list)
-        power=length
-        value=0
+        value = 0
 
-        for i in range(0,length):
-            if i!=0:
-                value+=self.coeff_list[i]* (agg.get_ID()**power)
-                print ("agg** power" , agg.get_ID() ** power)
-                print("power", power)
-                print("coeff", self.coeff_list[i])
-            power-=1
+        for i in range(0, len(self.bin_string)):
+            if self.bin_string[i] != 0:
+                value += self.coeff_list[i] * (agg.get_ID() ** power)
+            power -= 1
 
+        value += self.secret
         agg.shares_list.append(value)
-
 
     def get_poly(self):
         return self.polynomial
-
 
 
 def get_aggs():
@@ -91,34 +85,31 @@ def get_aggs():
     return aggregators
 
 
-
-
 def get_smart_meters():
     sm = int(input("How many smart meters? "))
     while sm < 1:
         sm = int(input("How many smart meters? "))
     return sm
 
+
 def main():
     aggregator_list = []
     smart_meter_list = []
 
+    aggregators = get_aggs()
+    degree = aggregators - 1
 
-    aggregators=get_aggs()
-    degree=aggregators-1
-
-
-    for i in range(1,aggregators+1):
-        a=Aggregator(i)
+    for i in range(1, aggregators + 1):
+        a = Aggregator(i)
         aggregator_list.append(a)
 
-    sm_num=get_smart_meters()
-    for i in range(1, sm_num+1):
-        secret=int(input("What is the secret for Smart Meter #" + str(i) + "? "))
+    sm_num = get_smart_meters()
+    for i in range(1, sm_num + 1):
+        secret = int(input("What is the secret for Smart Meter #" + str(i) + "? "))
         while secret < 1:
-            secret=int(input("What is the secret for Smart Meter #" + str(i) + "? "))
+            secret = int(input("What is the secret for Smart Meter #" + str(i) + "? "))
 
-        sm= SmartMeter(degree,secret,i)
+        sm = SmartMeter(degree, secret, i)
         smart_meter_list.append(sm)
 
     for sm in smart_meter_list:
@@ -137,4 +128,6 @@ def main():
 
     for a in aggregator_list:
         print("Shares list for aggregator #" + str(a.get_ID()) + ": " + a.print_shares_list())
+
+
 main()
